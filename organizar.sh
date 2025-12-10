7b55f87cdc7e7c55f049c98076d0460799b081c4
David Martins <168449686+davidmar0@users.noreply.github.com>
2025-12-10 10:59:49 +0000

    fix

--- organizar.sh
+++ organizar.sh
@@ -6,7 +6,7 @@ mkdir -p Imagens Videos Documentos Audio Arquivos Outros
 # Loop por todos os ficheiros na diretoria atual
 for ficheiro in *; do
     # Verifica se é um ficheiro e não o próprio script
-    if [[ -f "$ficheiro" && "$ficheiro" != "organizar_categorias.sh" ]]; then
+    if [[ -f "$ficheiro" && "$ficheiro" != "organizar.sh" ]]; then
 
     
 
